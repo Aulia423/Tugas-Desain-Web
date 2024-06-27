15400375d9c5ee9afdc3050c5091e4ecb48cdbd8
Aulia423 <sengokusama87@gmail.com>
2024-06-27 14:18:49 +0700

    first commit

---
+++ kalkulator/script.js
@@ -0,0 +1,27 @@
+function tambah() {
+    var angka1 = document.getElementById('angka1').value;
+    var angka2 = document.getElementById('angka2').value;
+    var hasil = parseInt(angka1) + parseInt(angka2);
+    document.getElementById('hasil').innerHTML = hasil;
+}
+
+function kurang() {
+    var angka1 = document.getElementById('angka1').value;
+    var angka2 = document.getElementById('angka2').value;
+    var hasil = parseInt(angka1) - parseInt(angka2);
+    document.getElementById('hasil').innerHTML = hasil;
+}
+
+function bagi() {
+    var angka1 = document.getElementById('angka1').value;
+    var angka2 = document.getElementById('angka2').value;
+    var hasil = parseInt(angka1) / parseInt(angka2);
+    document.getElementById('hasil').innerHTML = hasil;
+}
+
+function kali() {
+    var angka1 = document.getElementById('angka1').value;
+    var angka2 = document.getElementById('angka2').value;
+    var hasil = parseInt(angka1) * parseInt(angka2);
+    document.getElementById('hasil').innerHTML = hasil;
+}
